0a79ef5f72ec0983ae3f0a08f9c067ef4bafbe5b
FedeBGitHub <fedebarrionuevo76@gmail.com>
2025-03-25 13:22:25 -0230

    agrego diferentes datos por pantalla de una varible y un puntero.

--- tp1_1.c
+++ tp1_1.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 
 int main(){
-    printf("Hola mundo.\n");
+    //printf("Hola mundo.\n");
+    int num = 4;
+    int *punt;
+
+    punt = &num;
+
+    printf("El contenido del puntero es: %d.\n", *punt);
+    printf("La direccion de memoria almacenada por el puntero es: %p.\n", punt);
+    printf("La direccion de memoria de la variable es: %p\n", &num);
+    printf("La direccion de memoria del puntero es: %p\n", &punt);
+    printf("El tamaño de memoria utilizado por la variable num usando la funcion sizeof() es: %zu bytes.\n", sizeof(num));
     return 0;
 }
